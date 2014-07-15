27537f6a6ff8cfae7ecdd9e306ad458f8adb6e42
loipl <loiphamle@gmail.com>
2014-07-15 15:05:47 +0700

    add unit test

--- trunk/email/tests/unit_tests/TestThrottle.php
+++ trunk/email/tests/unit_tests/TestThrottle.php
@@ -395,4 +395,111 @@ class TestThrottle extends UnitTestCase
         }
     }
     //--------------------------------------------------------------------------
+    
+    
+    public function testThrottleSwithchingChannel()
+    {
+        // reset data
+        SetupTestData::resetQueueBuildData();
+        SetupTestData::resetThrottleData();
+        
+        // add test data
+        SetupTestData::addQueueBuildData(array(
+            array(
+                'email'         => 'test@edgeprod.com',
+                'stage'         => '1',
+                'campaign_id'   => '3',
+                'creative_id'   => '106',
+                'category_id'   => '7',
+                'from_name'     => 'Potato Express',
+                'sender_email'  => 'jason@matchquota.com',
+                'subject'       => 'Fast and Easy Potatoes',
+                'html_body'     => 'html body',
+                'text_body'     => 'text body',
+                'sub_id'        => '1',
+                'channel'       => NULL
+            ),
+            array(
+                'email'         => 'dom@leadwrench.com',
+                'stage'         => '1',
+                'campaign_id'   => '3',
+                'creative_id'   => '106',
+                'category_id'   => '7',
+                'from_name'     => 'Potato Express',
+                'sender_email'  => 'jason@matchquota.com',
+                'subject'       => 'Fast and Easy Potatoes',
+                'html_body'     => 'html body',
+                'text_body'     => 'text body',
+                'sub_id'        => '2',
+                'channel'       => NULL
+            ),
+            array(
+                'email'         => 'dom@edgeprod.com',
+                'stage'         => '1',
+                'campaign_id'   => '3',
+                'creative_id'   => '107',
+                'category_id'   => '8',
+                'from_name'     => 'Potato Express BOGO',
+                'sender_email'  => 'jason@matchquota.com',
+                'subject'       => 'Fast and Easy Potatoes',
+                'html_body'     => 'html body',
+                'text_body'     => 'text body',
+                'sub_id'        => '3',
+                'channel'       => NULL
+            ),
+        ));
+        
+        SetupTestData::addThrottleData(array(
+            array(
+                'type'          => Config::TRANSACTION_TYPE_HARDBOUNCE,
+                'domain'        => 'edgeprod.com',
+                'channel'       => '1',
+                'creative_id'   => '106',
+                'campaign_id'   => '3',
+                'category_id'   => '7'
+            ),
+            array(
+                'type'          => Config::TRANSACTION_TYPE_SOFTBOUNCE,
+                'domain'        => 'leadwrench.com',
+                'channel'       => '1',
+                'creative_id'   => '100',
+                'campaign_id'   => '5',
+                'category_id'   => '1'
+            ),
+            array(
+                'type'          => Config::TRANSACTION_TYPE_SOFTBOUNCE,
+                'domain'        => 'edgeprod.com',
+                'channel'       => '1',
+                'creative_id'   => '106',
+                'campaign_id'   => '3',
+                'category_id'   => '7'
+            ),
+            array(
+                'type'          => Config::TRANSACTION_TYPE_SOFTBOUNCE,
+                'domain'        => 'edgeprod.com',
+                'channel'       => '2',
+                'creative_id'   => '107',
+                'campaign_id'   => '3',
+                'category_id'   => '8'
+            )
+        ));
+        
+        // execute function
+        $leads = array(
+            0 => array('build_queue_id' => '1'),
+            1 => array('build_queue_id' => '2'),
+            2 => array('build_queue_id' => '3')
+        );
+        
+        Engine_Scheduler_Channels::pushChannelsToBuildQueue($leads);
+        
+        $queue1 = new Queue_Build('1');
+        $queue2 = new Queue_Build('2');
+        $queue3 = new Queue_Build('3');
+        
+        $this->assertEqual($queue1->getChannel(), '2');
+        $this->assertEqual($queue2->getChannel(), '2');
+        $this->assertEqual($queue3->getChannel(), '1');
+    }
+    //--------------------------------------------------------------------------
 }
